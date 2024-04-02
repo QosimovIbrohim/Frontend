38e852f2e245f54bb8798a393e58f69669241394
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-04-02 08:30:44 +0500

    ha

--- JS-Crud/script.js
+++ JS-Crud/script.js
@@ -1,11 +1,4 @@
-let defaultData = {
-    Image: "/images/tarvuz.jpg",
-    title: "Tarvuz",
-    desc: "Juda ham mazali poliz ekini."
-};
-var cards = [defaultData];
-showCards();
-var ids = 1;
+var cards = [];
 
 function addCard() {
     var card = {
@@ -21,24 +14,12 @@ function addCard() {
 function showCards() {
     var container = document.querySelector('.container');
     container.innerHTML = '';
-    cards.forEach(function(card, index) {
+    cards.forEach(function(card) {
         var cardElement = document.createElement('div');
         cardElement.classList.add('card');
         cardElement.style.width = '18rem';
 
-        cardElement.innerHTML = `
-            <p id="uuid${index}" style="display: none;"></p>
-            <img src="${card.Image}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${card.title}</h5>
-                <p class="card-text">${card.desc}</p>
-            </div>
-            <hr>
-            <div class="card-body flex f-c">
-                <a href="#" class="btn btn-danger">Delete</a>
-                <a href="#" class="btn btn-primary">Update</a>
-            </div>
-        `;
+        cardElement.innerHTML = "<img src="+card.Image+" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">"+card.title+"</h5><p class="card-text">"+card.desc"+</p></div><hr><div class="card-body flex f-c"><a href="#" class="btn btn-danger">Delete</a><a href="#" class="btn btn-primary">Update</a></div>";
 
         container.appendChild(cardElement);
     });
